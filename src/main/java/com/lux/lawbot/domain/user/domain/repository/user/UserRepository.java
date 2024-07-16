c8d6324494c05959743877ff2a5dda69a3baafef
최미래 <miraechoi@dgsw.hs.kr>
2024-07-16 23:03:51 +0900

    feat :: create user repository

---
+++ src/main/java/com/lux/lawbot/domain/user/domain/repository/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.lux.lawbot.domain.user.domain.repository.user;
+
+import com.lux.lawbot.domain.user.domain.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
