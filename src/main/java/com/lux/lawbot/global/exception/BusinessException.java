325b141d5eb082111a104ab772628b64098d545c
최미래 <miraechoi@dgsw.hs.kr>
2024-07-16 23:11:10 +0900

    feat :: create business exception

---
+++ src/main/java/com/lux/lawbot/global/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.lux.lawbot.global.exception;
+
+import com.lux.lawbot.global.exception.error.ErrorProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BusinessException extends RuntimeException {
+    private final ErrorProperty error;
+}
