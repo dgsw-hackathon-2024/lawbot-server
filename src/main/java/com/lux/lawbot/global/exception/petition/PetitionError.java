0c5ed615a810b8f5bcef5e1f6e6ff3d43d675a10
최미래 <miraechoi@dgsw.hs.kr>
2024-07-17 10:20:41 +0900

    refactor :: refactor petition error code

--- src/main/java/com/lux/lawbot/global/exception/petition/PetitionError.java
+++ src/main/java/com/lux/lawbot/global/exception/petition/PetitionError.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum PetitionError implements ErrorProperty {
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
-    PETITION_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 청원을 찾을 수 없습니다."),
-    PETITION_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "청원을 작성할 수 없습니다");
+    PETITION_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 청원을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
