be21c642406993eb2bdcb7bb568b1a08e8205d37
최미래 <miraechoi@dgsw.hs.kr>
2024-07-17 11:07:43 +0900

    feat :: create report entity

---
+++ src/main/java/com/lux/lawbot/domain/report/domain/entity/Report.java
@@ -0,0 +1,36 @@
+package com.lux.lawbot.domain.report.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "reports")
+public class Report {
+    @Id
+    private Long id;
+
+    /*
+        제목
+    */
+    @Column(nullable = false)
+    private String title;
+
+    /*
+        내용
+    */
+    @Column(nullable = false)
+    private String contents;
+
+    @Builder
+    public Report(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+}
